33e80e1919e1f5f28997d6701e8aff67e45aee35
Prateek Jain <jprateek601@gmail.com>
2022-01-08 12:16:37 +0530

    defined constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,9 +6,33 @@ const Color primaryColor2 = Color(0xff303F60);
 const Color secondaryColor1 = Color(0xffF5F9FD);
 const Color secondaryColor2 = Color(0xffCED3DC);
 
+// country list and news sources list
+const List<String> countryList = ['Australia', 'USA', 'India', 'South Africa', 'France', 'Germany'];
+const List<String> newsSources = ['ABC News', 'Business Insider', 'CNN', 'The Hindu', 'Google News (India)', 'Bloomberg'];
+
+// Map of country and country code
+const Map<String, String> countryMap = {
+  'Australia': 'au',
+  'USA': 'us',
+  'India': 'in',
+  'South Africa': 'za',
+  'France': 'fr',
+  'Germany': 'de'
+};
+
+// Map of news source and news source code
+const Map<String, String> newsSourceMap = {
+  'ABC News': 'abc-news',
+  'Business Insider': 'business-insider',
+  'CNN': 'cnn',
+  'The Hindu': 'the-hindu',
+  'Google News (India)': 'google-news-in',
+  'Bloomberg': 'bloomberg'
+};
+
 // user selected variable values
 class SelectedVar {
   static String country = 'in';
   static String sortBy = 'publishedAt';
-  static String sources = '';
+  static List sources = [];
 }
