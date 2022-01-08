68c9c8bb83d3819811b719ac1cf190d1725ef8f1
Prateek Jain <jprateek601@gmail.com>
2022-01-08 20:24:01 +0530

    fixed the ui of news cards in news list widget

--- lib/shared/news_list.dart
+++ lib/shared/news_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/constants.dart';
 import 'package:news_app/data_model/news.dart';
 
 typedef void OnMaxScrollExtent();
@@ -43,34 +44,55 @@ class _NewsListState extends State<NewsList> {
                   children: [
                     Expanded(
                       flex: 3,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            this.widget.articles[index].source.name,
-                            maxLines: 1,
-                          ),
-                          SizedBox(height: 5,),
-                          Text(
-                            this.widget.articles[index].title,
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 4,
-                          ),
-                          Text(
-                            this.widget.articles[index].publishedAt,
-                            maxLines: 1,
-                          )
-                        ],
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  this.widget.articles[index].source.name,
+                                  maxLines: 1,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                    fontStyle: FontStyle.italic
+                                  ),
+                                ),
+                                SizedBox(height: 5,),
+                                Text(
+                                  this.widget.articles[index].title,
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 3,
+                                ),
+                              ],
+                            ),
+                            Text(
+                              published(index),
+                              maxLines: 1,
+                              style: TextStyle(
+                                fontSize: 10,
+                                color: Colors.grey
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                     Expanded(
                         flex: 2,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(5),
-                          child: Image.network(
-                            this.widget.articles[index].urlToImage,
-                            fit: BoxFit.cover,
-                            height: double.infinity,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(5),
+                            child: Image.network(
+                              this.widget.articles[index].urlToImage,
+                              fit: BoxFit.cover,
+                              height: double.infinity,
+                            ),
                           ),
                         )
                     )
@@ -81,5 +103,25 @@ class _NewsListState extends State<NewsList> {
           );
         }
     );
+  }
+
+  String published(int index) {
+    DateTime dateTime = DateTime.parse(this.widget.articles[0].publishedAt);
+    var difference = DateTime.now().difference(dateTime);
+    if(difference.inDays > 7 ) {
+      return 'a week ago';
+    } else if(difference.inDays > 1) {
+      return '${difference.inDays} days ago';
+    } else if(difference.inDays > 0) {
+      return '${difference.inDays} day ago';
+    } else if(difference.inHours > 1) {
+      return '${difference.inHours} hours ago';
+    } else if(difference.inHours > 0) {
+      return '${difference.inHours} hour ago';
+    } else if(difference.inMinutes > 1) {
+      return '${difference.inDays} minutes ago';
+    } else {
+      return 'a moment ago';
+    }
   }
 }
